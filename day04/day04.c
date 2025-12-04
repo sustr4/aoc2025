b0ce6c7d8dbc1b7a564a2c581b2a021484993c84
Zdenek Sustr <git@sustr.net>
2025-12-04 06:11:41 +0100

    Task 1

---
+++ day04/day04.c
@@ -0,0 +1,146 @@
+#define _GNU_SOURCE
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include<limits.h>
+#include<assert.h>
+
+// Boundary and input file definitions, set as required
+#define INPUT "input.txt"
+#define MAXX 138
+#define MAXY 138
+//#define INPUT "unit1.txt"
+//#define MAXX 10
+//#define MAXY 10
+
+// Point structure definition
+typedef struct {
+	int x;
+	int y;
+	int z;
+} TPoint;
+
+// Comparator function example
+int comp(const void *a, const void *b)
+{
+	const int *da = (const int *) a;
+	const int *db = (const int *) b;
+	return (*da > *db) - (*da < *db);
+}
+
+// Example for calling qsort()
+//qsort(array,count,sizeof(),comp);
+
+
+// Print a two-dimensional array
+void printMap (char **map) {
+	int x,y;
+	for(y=0; y<MAXY; y++) {
+		for(x=0; x<MAXX; x++) {
+			printf("%c", map[y][x]);
+		}
+		printf("\n");
+	}
+}
+// Full block character for maps █ and border elements ┃━┗┛┏┓
+// Color printf("\033[1;31mR \033[1;32mG \033[1;34mB \033[0moff\n");
+
+// Retrieve nth neighbor from a map, diagonals are odd, side neighbors even
+int dy[] = { -1, -1, -1, 0, 1, 1,  1,  0};
+int dx[] = { -1,  0,  1, 1, 1, 0, -1, -1};
+char mapnb(char **map, int y, int x, int n) {
+	assert((n>=0) && (n<8));
+	if((y+dy[n]<0) || (y+dy[n]>=MAXY) ||
+	   (x+dx[n]<0) || (x+dx[n]>=MAXX)) return 0;
+	return(map[y+dy[n]][x+dx[n]]);
+}
+
+// Read input file line by line (e.g., into an array)
+char **readInput() {
+//int readInput() {
+	FILE * input;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	int count = 0;
+
+	input = fopen(INPUT, "r");
+	if (input == NULL) {
+		fprintf(stderr,"Failed to open input file\n");
+		exit(1); }
+
+	// Allocate one-dimensional array of strings
+	// char **inst=(char**)calloc(MAXX, sizeof(char*));
+	// TPoint *inst=(TPoint*)calloc(MAXX, sizeof(TPoint));
+
+	// Allocate a two-dimensional arrray of chars
+	int x=0, y=0;
+	char **map=calloc(MAXY,sizeof(char*));
+	for(int iter=0; iter<MAXY; iter++) map[iter]=calloc(MAXX,sizeof(char));
+
+	while ((read = getline(&line, &len, input)) != -1) {
+		line[strlen(line)-1] = 0; // Truncate the NL
+
+		// Read into map
+		for(x=0; x<MAXX; x++) map[y][x] = line[x];
+		y++;
+
+		// Copy to string
+		//asprintf(&(inst[count]), "%s", line);	
+
+		// Read into array
+		// sscanf(line,"%d,%d",
+		//	&(inst[count].x),
+		//	&(inst[count].y));
+
+		// Read tokens from single line
+		//char *token;
+		//token = strtok(line, ",");
+		//while( 1 ) {
+		//	if(!(token = strtok(NULL, ","))) break;
+		//}
+
+		count++;
+	}
+
+	fclose(input);
+	if (line) free(line);
+
+	return map;
+}
+
+int main(int argc, char *argv[]) {
+
+//	TPoint *array;
+//	int i=0;	
+//	array = readInput();
+	char **map = readInput();
+	int count=0;
+
+	int x,y,n;
+	for(y=0; y<MAXY; y++) {
+		for(x=0; x<MAXX; x++) {
+			int c=0;
+			if(map[y][x]!='@') continue;
+			for(n=0; n<8; n++) {
+				if('@'==mapnb(map, y, x, n)) c++;
+				if('x'==mapnb(map, y, x, n)) c++;
+				
+			}
+			if(c<4) {
+				count++;
+				map[y][x]='x';
+			}
+		}
+	}
+//	#pragma omp parallel for private(<uniq-var>) shared(<shared-var>)
+//	for(i=0; array[i]; i++) {
+//		printf("%d\n", array[i]);
+//	}
+
+
+	printMap(map);
+	printf("%d\n", count);
+	return 0;
+}
