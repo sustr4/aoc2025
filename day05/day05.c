20a4982029615a851174aea5e1b618fa67df2991
Zdenek Sustr <git@sustr.net>
2025-12-05 08:56:06 +0100

    Task 2 + code cleanup

--- day05/day05.c
+++ day05/day05.c
@@ -23,36 +23,9 @@ typedef struct {
 // Comparator function example
 int comp(const void *a, const void *b)
 {
-	const int *da = (const int *) a;
-	const int *db = (const int *) b;
-	return (*da > *db) - (*da < *db);
-}
-
-// Example for calling qsort()
-//qsort(array,count,sizeof(),comp);
-
-
-// Print a two-dimensional array
-void printMap (char **map) {
-	int x,y;
-	for(y=0; y<MAXY; y++) {
-		for(x=0; x<MAXX; x++) {
-			printf("%c", map[y][x]);
-		}
-		printf("\n");
-	}
-}
-// Full block character for maps █ and border elements ┃━┗┛┏┓
-// Color printf("\033[1;31mR \033[1;32mG \033[1;34mB \033[0moff\n");
-
-// Retrieve nth neighbor from a map, diagonals are odd, side neighbors even
-int dy[] = { -1, -1, -1, 0, 1, 1,  1,  0};
-int dx[] = { -1,  0,  1, 1, 1, 0, -1, -1};
-char mapnb(char **map, int y, int x, int n) {
-	assert((n>=0) && (n<8));
-	if((y+dy[n]<0) || (y+dy[n]>=MAXY) ||
-	   (x+dx[n]<0) || (x+dx[n]>=MAXX)) return 0;
-	return(map[y+dy[n]][x+dx[n]]);
+	const TFresh *da = (const TFresh *) a;
+	const TFresh *db = (const TFresh *) b;
+	return (da->from > db->from) - (da->from < db->from);
 }
 
 // Read input file line by line (e.g., into an array)
@@ -69,13 +42,6 @@ int readInput(TFresh *fresh, long long int *ing) {
 		fprintf(stderr,"Failed to open input file\n");
 		exit(1); }
 
-	// Allocate one-dimensional array of strings
-
-	// Allocate a two-dimensional arrray of chars
-	// int x=0, y=0;
-	// char **map=calloc(MAXY,sizeof(char*));
-	// for(int iter=0; iter<MAXY; iter++) map[iter]=calloc(MAXX,sizeof(char));
-
 	int ingr=1;
 
 	while ((read = getline(&line, &len, input)) != -1) {
@@ -86,12 +52,6 @@ int readInput(TFresh *fresh, long long int *ing) {
 			count=0;
 			continue;
 		}
-
-		// Read into map
-		// for(x=0; x<MAXX; x++) map[y][x] = line[x];
-		// y++;
-
-		// Copy to string
 
 		if(ingr) {
 			// Read into array
@@ -105,65 +65,47 @@ int readInput(TFresh *fresh, long long int *ing) {
 				&(ing[count]));
 		}
 
-		// Read tokens from single line
-		//char *token;
-		//token = strtok(line, ",");
-		//while( 1 ) {
-		//	if(!(token = strtok(NULL, ","))) break;
-		//}
-
 		count++;
 	}
 
 	fclose(input);
 	if (line) free(line);
 
-//	printMap(map);
-
 	return 0;
-//	return inst;
-//	return map;
 }
 
 int main(int argc, char *argv[]) {
 
 	TFresh *fresh=(TFresh*)calloc(MAXX, sizeof(TFresh));
 	long long *ing=(long long*)calloc(MAXY, sizeof(TFresh));
-//	TFresh *array;
 	int i=0;	
 	long long int count=0;
 	readInput(fresh, ing);
 
-//	#pragma omp parallel for private(<uniq-var>) shared(<shared-var>)
+	for(i=0; fresh[i].from; i++);
+
+	qsort(fresh,i,sizeof(TFresh),comp);
 	int change=1;
 	while(change) {
 		change=0;
 		for(i=0; i<MAXX-1; i++) {
 			if((fresh[i].from==0)||(fresh[i].to==0)) continue;
-	//		printf("%lld-%lld\n", fresh[i].from, fresh[i].to);
 			for(int y=i+1; y<MAXX; y++) {
 				if((fresh[y].from==0)||(fresh[y].to==0)) continue;
 				if((fresh[i].from<=fresh[y].from) && (fresh[i].to>=fresh[y].from)) { // y Start falls into i
-					if(fresh[y].to<=fresh[i].to) { // y end also falls into i
-						fresh[y].from=0; fresh[y].to=0;
-					}
-					else {
+					if(fresh[y].to>fresh[i].to) { // y end also falls into i
 						fresh[i].to=fresh[y].to;
-						fresh[y].from=0; fresh[y].to=0;
 						change++;
 					}
+					fresh[y].from=0; fresh[y].to=0;
 				}
 				if((fresh[i].from<=fresh[y].to) && (fresh[i].to>=fresh[y].to)) { // y end falls into i
-					if(fresh[y].from>=fresh[i].from) { // y start also falls into i
-						fresh[y].from=0; fresh[y].to=0;
-					}
-					else {
+					if(fresh[y].from<fresh[i].from) { // y start also falls into i
 						fresh[i].from=fresh[y].from;
-						fresh[y].from=0; fresh[y].to=0;
 						change++;
 					}
+					fresh[y].from=0; fresh[y].to=0;
 				}
-	//			printf("\t%lld-%lld\n", fresh[y].from, fresh[y].to);
 			}
 		}
 	}
@@ -173,7 +115,6 @@ int main(int argc, char *argv[]) {
 		printf("%lld-%lld\n", fresh[i].from, fresh[i].to);
 		count+=fresh[i].to-fresh[i].from+1;
 	}
-	printf("358972987438930 too high\n");
 	printf("%lld fresh\n", count);
 
 	return 0;
